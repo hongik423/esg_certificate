f6bfffe5dc3ef933576ed463ec862544e572e819
hongik423 <hongik423@gmail.com>
2025-07-11 10:29:27 +0900

    Restore original M-CENTER version - remove ESG branding

---
+++ esg_certification_center/src/lib/config/branding.ts
@@ -0,0 +1,51 @@
+export const branding = {
+  // 회사 정보
+  companyName: 'ESG 인증원',
+  companyNameEn: 'ESG Certification Center',
+  ceo: '박윤철',
+  ceoTitle: '대표이사',
+  
+  // 연락처 정보 (모든 연락처는 이후경 경영지도사)
+  contactPerson: '이후경 경영지도사',
+  phone: '010-9251-9743',
+  email: 'hongik423@gmail.com',
+  
+  // 운영 시간
+  operatingHours: '평일/주말 09:00-21:00',
+  responseTime: '24시간 이내',
+  
+  // 주소
+  address: '서울특별시 강남구 테헤란로 123',
+  
+  // 웹사이트
+  websiteUrl: 'https://esg-certification.co.kr',
+  
+  // 로고
+  logoPath: '/esgr_logo.svg',
+  logoAlt: 'ESG 인증원 Logo',
+  
+  // 핵심 가치
+  coreValues: [
+    '공평성',
+    '전문성', 
+    '신뢰성',
+    '혁신성'
+  ],
+  
+  // 서비스 슬로건
+  slogan: '지속가능한 미래를 위한 ESG 인증 전문기관',
+  
+  // 미션
+  mission: 'ESG 경영 시스템 인증을 통해 기업의 지속가능한 성장을 지원합니다.',
+  
+  // 비전
+  vision: 'ESG 인증 분야 대한민국 대표 기관',
+  
+  // 소셜 미디어 (필요시 추가)
+  social: {
+    // facebook: '',
+    // instagram: '',
+    // youtube: '',
+    // blog: ''
+  }
+}; 
